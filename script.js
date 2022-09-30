41a8bd610526fa53219689269903d522173b09ef
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-09-30 21:20:10 +0100

    Add functions to display books into webpage from myLibrary array

--- script.js
+++ script.js
@@ -22,3 +22,47 @@ function addBookToLibrary(title, author, pages) {
 
   myLibrary.push(newBook)
 }
+
+/**
+ * Displays a 'Book Card' for every book object in myLibrary,
+ */
+function displayBooks() {
+  myLibrary.forEach(function(book) {
+      addBookCard(book)
+  })
+}
+
+/**
+ * Creates a 'Book card' and adds it to the DOM
+ * 
+ * @param {Book} book    The book object to be added
+ */
+function addBookCard(book) {
+  // create div
+  const div = document.createElement('div')
+  // set class to book
+  div.className = 'book'
+  // add div to bc
+  document.getElementById('books-container').appendChild(div)
+
+  // create ul
+  const ul = document.createElement('ul')
+  // add ul to div
+  div.appendChild(ul)
+
+  // create li's
+  const title = document.createElement('li')
+  const author = document.createElement('li')
+  const pages = document.createElement('li')
+  const read = document.createElement('li')
+
+  title.innerHTML = 'Title: ' + book.title
+  author.innerHTML = 'Author: ' + book.author
+  pages.innerHTML = 'Pages: ' + book.pages
+  read.innerHTML = 'Status: ' + book.read ? 'Read' : 'Not read'
+  // add li's to ul
+  ul.appendChild(title)
+  ul.appendChild(author)
+  ul.appendChild(pages)
+  ul.appendChild(read)
+}
