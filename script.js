f380b202dac26ba5625dfdb42983baf38020aca4
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-09-30 19:53:16 +0100

    Add addBookToLibrary function to script.js
    Add the ability to add new books to myLibrary.
    The function requires another function to get input from the user
    to populate its params.

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function Book(title, author, pages) {
 Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`
 }
+
+/**
+ * Stores user's input as a new Book object into myLibrary,
+ * @param {string} title    Book's title.
+ * @param {string} author   Book's author name.
+ * @param {number} pages    Book's number of pages.
+ */
+function addBookToLibrary(title, author, pages) {
+  const newBook = new Book(title, author, pages)
+
+  myLibrary.push(newBook)
+}
